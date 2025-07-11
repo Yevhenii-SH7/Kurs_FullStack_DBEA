ecb4ce77b219d707f40da548cda518459937f20b
Yevhenii-SH7 <aertsfun@gmail.com>
2025-07-11 10:16:22 +0200

    feat: add info modal window

--- ZutatenZirkus/main.js
+++ ZutatenZirkus/main.js
@@ -2,17 +2,43 @@ const portionsmenge = 4;
 const zutatenMenge = 200;
 const berechneteMenge = zutatenMenge * portionsmenge;
 
-if(portionsmenge <= 0) {
-    alert("Portionsmenge: " + portionsmenge);
+if (portionsmenge <= 0) {
+  alert("Portionsmenge: " + portionsmenge);
 } else {
-console.log(`Berechnete Portionsmenge: ${berechneteMenge}`);
+  console.log(`Berechnete Portionsmenge: ${berechneteMenge}`);
 }
 
-const zutaten = ["Huhn", "Currypulver", "Kokos milch", "Ingwer", "Zwiebel", "Reis", "Zitronensaft", "Chili"];
+const zutaten = [
+  "Huhn",
+  "Currypulver",
+  "Kokos milch",
+  "Ingwer",
+  "Zwiebel",
+  "Reis",
+  "Zitronensaft",
+  "Chili",
+];
 const mengen = [1, 10, 250, 2, 1, 200, 20, 0.5];
 const einheiten = ["Stk.", "g", "ml", "ml", "Stk.", "g", "ml", "g"];
 console.log(`Zutaten: ${zutaten[0]} ${mengen[0]} ${einheiten[0]}`);
 
 for (let i = 0; i < zutaten.length; i++) {
-    console.log(`Zutaten: ${zutaten[i]} ${mengen[i]} ${einheiten[i]}`);
+  console.log(`Zutaten: ${zutaten[i]} ${mengen[i]} ${einheiten[i]}`);
 }
+
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".tooltip");
+const closeModal = document.querySelector(".close-modal");
+openModal.addEventListener("click", () => {
+  modal.classList.toggle("hidden");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.toggle("hidden");
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.classList.toggle("hidden");
+  }
+});
